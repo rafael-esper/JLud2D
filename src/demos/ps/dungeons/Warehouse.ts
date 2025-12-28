f616184713c410f071dc972972c7eba324db9167
Rafael <rfpereira3@gmail.com>
2025-12-28 18:55:58 -0300

    Warehouse trying to open chest

--- src/demos/ps/dungeons/Warehouse.ts
+++ src/demos/ps/dungeons/Warehouse.ts
@@ -7,23 +7,28 @@ import { PSGame } from '../PSGame';
 import { Chest, Flags, Trapped } from '../game/GameData';
 import { OriginalItem } from '../game/PSLibItem';
 import { City } from '../game/City';
+import { MainEngine } from '../../../core/MainEngine';
 
 export class Warehouse {
 
   public static async startmap(): Promise<void> {
+    // Set script context to this class so chest methods can be found
+    MainEngine.setScriptContext(Warehouse);
+    console.log('Warehouse: Script context set to Warehouse class');
+
     const dungeon = PSGame.getCurrentDungeonInstance();
     if (dungeon) {
       await dungeon.startDungeon();
     }
   }
 
-  public static chest1(): void {
-    PSGame.chestFlag(Chest.WAREHOUSE_CHEST1, 50, Trapped.NO_TRAP, null);
+  public static async chest1(): Promise<void> {
+    await PSGame.chestFlag(Chest.WAREHOUSE_CHEST1, 50, Trapped.NO_TRAP, null);
   }
 
-  public static chestKey(): void {
+  public static async chestKey(): Promise<void> {
     if (PSGame.hasFlag(Flags.INFO_KEY)) {
-      PSGame.chestFlag(Chest.WAREHOUSE_CHESTKEY, 0, Trapped.NO_TRAP, PSGame.getItem(OriginalItem.Quest_Dungeon_Key));
+      await PSGame.chestFlag(Chest.WAREHOUSE_CHESTKEY, 0, Trapped.NO_TRAP, PSGame.getItem(OriginalItem.Quest_Dungeon_Key));
     }
   }
 
